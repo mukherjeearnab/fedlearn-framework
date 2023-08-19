dbd7718a0ba4caf30d6f02dd5298f80537dc6e98
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2023-08-19 12:32:13 +0530

    Added random distribution template

--- server/templates/distribution/random_dist.py
+++ server/templates/distribution/random_dist.py
@@ -1,4 +1,33 @@
 '''
-Creates a distribution of dataset of 1 Client.
+Creates a random distribution of dataset of k clients.
 returns as a list of tensors
 '''
+import torch
+
+
+def distribute_into_client_chunks(dataset: tuple, client_weights: list) -> list:
+    '''
+    Creates random chunks by splitting the original dataset into 
+    len(client_weights) chunks, based on the client weights.
+    '''
+
+    data, labels = dataset
+
+    total_data_samples = len(data)
+
+    # calculate the split sections
+    split_sections = [int(total_data_samples*weight)
+                      for weight in client_weights]
+
+    # split the data and labels into chunks
+    data_chunks = torch.split(data, split_size_or_sections=split_sections)
+    label_chunks = torch.split(labels, split_size_or_sections=split_sections)
+
+    # create dataset tuples for client chunks
+    client_chunks = []
+    for i in range(len(client_weights)):
+        client_chunk = (data_chunks[i], label_chunks[i])
+
+        client_chunks.append(client_chunk)
+
+    return client_chunks
