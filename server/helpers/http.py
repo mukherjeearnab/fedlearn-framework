b9a1d60a13a88a6c8dcec3f4730a5d39c88e0e16
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2023-08-26 15:27:01 +0530

    Increased Timeout for HTTP

--- server/helpers/http.py
+++ server/helpers/http.py
@@ -8,7 +8,7 @@ def get(url: str, params: dict) -> dict:
     '''
     GET request method
     '''
-    req = requests.get(url, params, timeout=10)
+    req = requests.get(url, params, timeout=3600)
     data = req.json()
 
     return data
@@ -18,7 +18,7 @@ def post(url: str, params: dict) -> dict:
     '''
     POST request method
     '''
-    req = requests.post(url, json=params, timeout=10)
+    req = requests.post(url, json=params, timeout=3600)
     data = req.json()
 
     return data
