91393ea6bf916e75d3e170f3a373eba7ce2181ff
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2023-12-26 12:01:04 +0530

    Added GCON

--- templates/training/cel_gcon.py
+++ templates/training/cel_gcon.py
@@ -1,6 +1,7 @@
 '''
 Simple Neural Network Training Loop Module
 '''
+from copy import deepcopy
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -15,14 +16,16 @@ def train_loop(num_epochs: int, learning_rate: float,
     '''
 
     # hyperparameters
-    w_con = extra_params['conr']['w_con']
-    w_sup = extra_params['conr']['w_sup']
-    decay = extra_params['conr']['decay']
+    w_con = extra_params['gcon']['w_con']['init']
+    w_con_max = extra_params['gcon']['w_con']['max']
+    w_con_min = extra_params['gcon']['w_con']['min']
+    decay_step = extra_params['gcon']['w_con']['decay']['step']
+    decay_wait = extra_params['gcon']['w_con']['decay']['wait']
 
     # extra_data
-    curr_round, total_round = float(extra_data['round_info']['current_round']), float(
-        extra_data['round_info']['total_rounds'])
-    decay_value = 1 - ((curr_round-1)/total_round) if decay else 1.0
+    extra_data['loss_history'] = {"prev": float(
+        'inf'), "redn_count": 0} if 'loss_history' not in extra_data else extra_data['loss_history']
+    extra_data['w_con'] = w_con if 'w_con' not in extra_data else extra_data['w_con']
 
     # move the local_model to the device, cpu or gpu
     local_model = local_model.to(device)
@@ -31,13 +34,14 @@ def train_loop(num_epochs: int, learning_rate: float,
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(local_model.parameters(), lr=learning_rate)
-
     cos = torch.nn.CosineSimilarity(dim=-1)
 
     # Epoch loop
     for epoch in range(num_epochs):
         local_model.train()
         total_loss = 0.0
+
+        w_con = 0.0 if epoch == 0 else extra_data['w_con']
 
         for i, (inputs, labels) in enumerate(train_loader, 1):
 
@@ -49,10 +53,9 @@ def train_loop(num_epochs: int, learning_rate: float,
             proj_g, _ = global_model.forward_with_projection(inputs)
 
             contrast = cos(proj_l, proj_g)
+            con_loss = w_con * torch.mean((1 - contrast))
 
-            con_loss = (decay_value * w_con) * torch.mean(1 - contrast)
-
-            sup_loss = w_sup * criterion(pred_l, labels)
+            sup_loss = criterion(pred_l, labels)
 
             loss = sup_loss + con_loss
 
@@ -63,4 +66,21 @@ def train_loop(num_epochs: int, learning_rate: float,
             print(f'Processing Batch {i}/{len(train_loader)}.', end='\r')
 
         average_loss = total_loss / len(train_loader)
-        print(f"Epoch [{epoch + 1}/{num_epochs}] - Loss: {average_loss:.4f}")
+        print(
+            f"Epoch [{epoch + 1}/{num_epochs}] - Loss: {average_loss:.4f} - w_con: {w_con:.2f}")
+
+        if epoch == 0:
+            if average_loss < extra_data['loss_history']['prev']:
+                if extra_data['loss_history']['redn_count'] >= decay_wait:
+                    extra_data['w_con'] -= decay_step
+                    if extra_data['w_con'] < w_con_min:
+                        extra_data['w_con'] = w_con_min
+                else:
+                    extra_data['loss_history']['redn_count'] += 1
+            else:
+                extra_data['loss_history']['redn_count'] = 0
+                extra_data['w_con'] += decay_step
+                if extra_data['w_con'] >= w_con_max:
+                    extra_data['w_con'] = w_con_max
+
+            extra_data['loss_history']['prev'] = average_loss
